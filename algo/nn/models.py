9719b1cc183cfab4126b24511f6ab1cf714bb802
TharinduDR <rhtdranasinghe@gmail.com>
2019-06-27 12:06:01 +0100

    001: Initial commit

---
+++ algo/nn/models.py
@@ -0,0 +1,132 @@
+import keras.backend as K
+from keras import Input, Model
+from keras.initializers import glorot_normal, orthogonal
+from keras.layers import Embedding, SpatialDropout1D, Bidirectional, GRU, Flatten, Dense, Dropout, BatchNormalization, \
+    Reshape, Conv2D, MaxPool2D, Concatenate, GlobalAveragePooling1D, GlobalMaxPooling1D, concatenate, LSTM
+from keras.optimizers import Adam
+
+from algo.nn.layers import Capsule, Attention
+
+
+def capsule(maxlen, max_features, embed_size, embedding_matrix):
+    K.clear_session()
+    inp = Input(shape=(maxlen,))
+    #     word = Embedding(max_features, embed_size, weights=[embedding_matrix], trainable=False)(inp)
+    #     emoji = Embedding(max_features, embed_size, weights=[emoji_embedding_matrix], trainable=False)(inp)
+    #     x = concatenate([(word), (emoji)])
+
+    x = Embedding(max_features, embed_size, weights=[embedding_matrix], trainable=False)(inp)
+
+    x = SpatialDropout1D(rate=0.2)(x)
+    #     x = Bidirectional(LSTM(64, return_sequences=True))(x)
+    x = Bidirectional(GRU(100, return_sequences=True,
+                          kernel_initializer=glorot_normal(seed=12300),
+                          recurrent_initializer=orthogonal(gain=1.0, seed=10000)))(x)
+
+    x = Capsule(num_capsule=10, dim_capsule=10, routings=4, share_weights=True)(x)
+    x = Flatten()(x)
+
+    x = Dense(100, activation="relu", kernel_initializer=glorot_normal(seed=12300))(x)
+    x = Dropout(0.12)(x)
+    x = BatchNormalization()(x)
+
+    x = Dense(1, activation="sigmoid")(x)
+    model = Model(inputs=inp, outputs=x)
+    model.compile(loss='binary_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+    return model
+
+
+def cnn_2d(maxlen, max_features, embed_size, embedding_matrix):
+    K.clear_session()
+
+    filter_sizes = [1, 2, 3, 5]
+    num_filters = 32
+
+    inp = Input(shape=(maxlen,))
+    x = Embedding(max_features, embed_size, weights=[embedding_matrix])(inp)
+    x = SpatialDropout1D(0.4)(x)
+    x = Reshape((maxlen, embed_size, 1))(x)
+
+    conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0], embed_size), kernel_initializer='normal',
+                    activation='elu')(x)
+    conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embed_size), kernel_initializer='normal',
+                    activation='elu')(x)
+    conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2], embed_size), kernel_initializer='normal',
+                    activation='elu')(x)
+    conv_3 = Conv2D(num_filters, kernel_size=(filter_sizes[3], embed_size), kernel_initializer='normal',
+                    activation='elu')(x)
+
+    maxpool_0 = MaxPool2D(pool_size=(maxlen - filter_sizes[0] + 1, 1))(conv_0)
+    maxpool_1 = MaxPool2D(pool_size=(maxlen - filter_sizes[1] + 1, 1))(conv_1)
+    maxpool_2 = MaxPool2D(pool_size=(maxlen - filter_sizes[2] + 1, 1))(conv_2)
+    maxpool_3 = MaxPool2D(pool_size=(maxlen - filter_sizes[3] + 1, 1))(conv_3)
+
+    z = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2, maxpool_3])
+    z = Flatten()(z)
+    z = Dropout(0.1)(z)
+
+    outp = Dense(1, activation="sigmoid")(z)
+
+    model = Model(inputs=inp, outputs=outp)
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    return model
+
+
+def pooled_gru(maxlen, max_features, embed_size, embedding_matrix):
+    K.clear_session()
+    inp = Input(shape=(maxlen,))
+    x = Embedding(max_features, embed_size, weights=[embedding_matrix])(inp)
+    x = SpatialDropout1D(0.2)(x)
+    x = Bidirectional(GRU(80, return_sequences=True))(x)
+    avg_pool = GlobalAveragePooling1D()(x)
+    max_pool = GlobalMaxPooling1D()(x)
+    conc = concatenate([avg_pool, max_pool])
+    outp = Dense(1, activation="sigmoid")(conc)
+
+    model = Model(inputs=inp, outputs=outp)
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    return model
+
+
+def lstm_attention(maxlen, max_features, embed_size, embedding_matrix):
+    K.clear_session()
+    inp = Input(shape=(maxlen,))
+    x = Embedding(max_features, embed_size, weights=[embedding_matrix], trainable=False)(inp)
+    x = Bidirectional(LSTM(64, return_sequences=True))(x)
+    x = Bidirectional(LSTM(64, return_sequences=True))(x)
+    x = Attention(maxlen)(x)
+    x = Dense(256, activation="relu")(x)
+    # x = Dropout(0.25)(x)
+    x = Dense(1, activation="sigmoid")(x)
+    model = Model(inputs=inp, outputs=x)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    return model
+
+
+def lstm_gru_attention(maxlen, max_features, embed_size, embedding_matrix):
+    inp = Input(shape=(maxlen,))
+    x = Embedding(max_features, embed_size, weights=[embedding_matrix], trainable=False)(inp)
+    x = SpatialDropout1D(0.1)(x)
+    x = Bidirectional(LSTM(40, return_sequences=True))(x)
+    y = Bidirectional(GRU(40, return_sequences=True))(x)
+
+    atten_1 = Attention(maxlen)(x)  # skip connect
+    atten_2 = Attention(maxlen)(y)
+    avg_pool = GlobalAveragePooling1D()(y)
+    max_pool = GlobalMaxPooling1D()(y)
+
+    conc = concatenate([atten_1, atten_2, avg_pool, max_pool])
+    conc = Dense(16, activation="relu")(conc)
+    conc = Dropout(0.1)(conc)
+    outp = Dense(1, activation="sigmoid")(conc)
+
+    model = Model(inputs=inp, outputs=outp)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    return model
---
+++ run_experiment.py
@@ -0,0 +1,137 @@
+import numpy as np
+import pandas as pd
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+from keras_preprocessing.sequence import pad_sequences
+from keras_preprocessing.text import Tokenizer
+from numpy.random import seed
+from sklearn.metrics import confusion_matrix, f1_score, accuracy_score, recall_score, precision_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import LabelEncoder
+from tensorflow import set_random_seed
+
+from algo.nn.models import capsule
+from algo.nn.utility import f1_smart
+from embeddings import get_emb_matrix
+from preprocessing import clean_text
+
+if __name__ == "__main__":
+    seed(726)
+    set_random_seed(726)
+
+    print('Reading files')
+
+    # Reading File Section - This should change
+    train = pd.read_csv("data/english/offenseval-training-v1.tsv", sep='\t')
+
+    test_tweets = pd.read_csv("data/english/testset-taska.tsv", sep='\t')
+    test_labels = pd.read_csv("data/english/labels-test-a.csv", header=-1, names=["id", "subtask_a"])
+
+    test = pd.merge(test_tweets, test_labels, on=['id', 'id'])
+
+    print('Completed reading')
+
+    #############
+    print("Train shape : ", train.shape)
+    print("Test shape : ", test.shape)
+
+    # Variables
+
+    TEXT_COLUMN = "tweet"
+    LABEL_COLUMN = "subtask_a"
+    EMBEDDING_FILE = '/data/fasttext/crawl-300d-2M-subword.vec'
+    MODEL_PATH = "models/capsule_net_weights_best.h5"
+    PREDICTION_FILE = "predictions/capsule_net.csv"
+
+    train[TEXT_COLUMN] = train[TEXT_COLUMN].str.lower()
+    test[TEXT_COLUMN] = test[TEXT_COLUMN].str.lower()
+
+    train[TEXT_COLUMN] = train[TEXT_COLUMN].apply(lambda x: clean_text(x))
+    test[TEXT_COLUMN] = test[TEXT_COLUMN].apply(lambda x: clean_text(x))
+
+    train['doc_len'] = train[TEXT_COLUMN].apply(lambda words: len(words.split(" ")))
+    max_seq_len = np.round(train['doc_len'].mean() + train['doc_len'].std()).astype(int)
+
+    embed_size = 300  # how big is each word vector
+    max_features = None  # how many unique words to use (i.e num rows in embedding vector)
+    maxlen = max_seq_len  # max number of words in a question to use #99.99%
+
+    # fill up the missing values
+    X = train[TEXT_COLUMN].fillna("_na_").values
+    X_test = test[TEXT_COLUMN].fillna("_na_").values
+
+    # Tokenize the sentences
+    tokenizer = Tokenizer(num_words=max_features, filters='')
+    tokenizer.fit_on_texts(list(X))
+
+    X = tokenizer.texts_to_sequences(X)
+    X_test = tokenizer.texts_to_sequences(X_test)
+
+    # Pad the sentences
+    X = pad_sequences(X, maxlen=maxlen)
+    X_test = pad_sequences(X_test, maxlen=maxlen)
+
+    # Get the target values
+    Y = train[LABEL_COLUMN].values
+
+    le = LabelEncoder()
+
+    le.fit(Y)
+    encoded_Y = le.transform(Y)
+
+    word_index = tokenizer.word_index
+    max_features = len(word_index) + 1
+
+    print('Loading Embeddings')
+
+    embedding_matrix = get_emb_matrix(word_index, max_features, EMBEDDING_FILE)
+
+    print('Finished loading Embeddings')
+
+    print('Start Training')
+
+    kfold = StratifiedKFold(n_splits=5, random_state=10, shuffle=True)
+    bestscore = []
+    y_test = np.zeros((X_test.shape[0],))
+    for i, (train_index, valid_index) in enumerate(kfold.split(X, encoded_Y)):
+        X_train, X_val, Y_train, Y_val = X[train_index], X[valid_index], encoded_Y[train_index], encoded_Y[valid_index]
+        filepath = MODEL_PATH
+        checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=2, save_best_only=True, mode='min')
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.6, patience=1, min_lr=0.0001, verbose=2)
+        earlystopping = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=2, verbose=2, mode='auto')
+        callbacks = [checkpoint, reduce_lr]
+        model = capsule(maxlen, max_features, embed_size, embedding_matrix)
+        if i == 0: print(model.summary())
+        model.fit(X_train, Y_train, batch_size=64, epochs=20, validation_data=(X_val, Y_val), verbose=2,
+                  callbacks=callbacks,
+                  )
+        model.load_weights(filepath)
+        y_pred = model.predict([X_val], batch_size=64, verbose=2)
+        y_test += np.squeeze(model.predict([X_test], batch_size=64, verbose=2)) / 5
+        f1, threshold = f1_smart(np.squeeze(Y_val), np.squeeze(y_pred))
+        print('Optimal F1: {:.4f} at threshold: {:.4f}'.format(f1, threshold))
+        bestscore.append(threshold)
+
+    print('Finished Training')
+
+    y_test = y_test.reshape((-1, 1))
+    pred_test_y = (y_test > np.mean(bestscore)).astype(int)
+    test['predictions'] = le.inverse_transform(pred_test_y)
+
+    # save predictions
+    file_path = PREDICTION_FILE
+    test.to_csv(file_path, sep='\t', encoding='utf-8')
+
+    print('Saved Predictions')
+
+    # post analysis
+    tn, fp, fn, tp = confusion_matrix(test[LABEL_COLUMN], test['predictions']).ravel()
+    weighted_f1 = f1_score(test[LABEL_COLUMN], test['predictions'], average='weighted')
+    accuracy = accuracy_score(test[LABEL_COLUMN], test['predictions'])
+    weighted_recall = recall_score(test[LABEL_COLUMN], test['predictions'], average='weighted')
+    weighted_precision = precision_score(test[LABEL_COLUMN], test['predictions'], average='weighted')
+
+    print("Confusion Matrix (tn, fp, fn, tp) {} {} {} {}".format(tn, fp, fn, tp))
+    print("Accuracy ", accuracy)
+    print("Weighted F1 ", weighted_f1)
+    print("Weighted Recall ", weighted_recall)
+    print("Weighted Precision ", weighted_precision)
